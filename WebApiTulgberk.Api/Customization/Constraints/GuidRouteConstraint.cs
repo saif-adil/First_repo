e5b6cd833150c25167eb0b4721cc34bacc624d5d
saif <saif.red5india@gmail.com>
2014-09-19 10:57:13 +0530

    git ignore file

---
+++ WebApiTulgberk.Api/Customization/Constraints/GuidRouteConstraint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace WebApiTulgberk.Api.Customization.Constraints
+{
+    public class GuidRouteConstraint: IHttpRouteConstraint
+    {
+        public bool Match(System.Net.Http.HttpRequestMessage request, IHttpRoute route, string parameterName, IDictionary<string, object> values, HttpRouteDirection routeDirection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApiTulgberk.Api/Customization/Handlers/HttpsRequiredMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApiTulgberk.Api.Customization.Handlers
+{
+    public class HttpsRequiredMessageHandler: DelegatingHandler
+    {
+
+        protected  override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if(request.RequestUri.Scheme    ==  Uri.UriSchemeHttps)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+                response.ReasonPhrase = "use https only";
+               return Task.FromResult<HttpResponseMessage>(response);
+            }
+
+            return base.SendAsync(request,cancellationToken);
+        }
+    }
+}
